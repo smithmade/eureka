9411689980e578405f6b58f8cec3356ea7e1bbd9
Nicholas Villapiano <nick@smithmade.org>
2014-04-08 11:41:36 -0400

    Added Analytics to index.php and header.php

--- header.php
+++ header.php
@@ -5,3 +5,16 @@
 		<a class="nav-about" href="about">about</a>
 		<a class="nav-contact" href="contact">contact</a>
 	</nav>
+
+<!-- analytics -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49827976-1', 'eureka601.com');
+  ga('send', 'pageview');
+
+</script>
